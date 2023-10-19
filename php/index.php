ff257575767f37baa1a1460638f6fbb082b20fa5
Maarten-Marx <r0929437@student.thomasmore.be>
2023-10-19 13:46:30 +0200

    Add basic documentation to PHP code

--- php/index.php
+++ php/index.php
@@ -9,27 +9,34 @@
 </head>
 <body>
     <?php
+    # Function to get my name from the database; takes a connection, returns a name.
     function get_name($conn) {
+        # Prepare the sql query and execute it.
         $sql = 'SELECT name FROM names limit 1';
         $stmt = mysqli_prepare($conn, $sql);
 
         mysqli_stmt_execute($stmt);
 
+        # Get the resulting name and return it.
         $result = mysqli_stmt_get_result($stmt);
         return $result -> fetch_row()[0];
     }
 
     try {
+        # Connect to the remote database and get the name from it, then show it on the web page.
         $conn = mysqli_connect("sql:3306", "root", "password", "apache");
         $name = get_name($conn);
 
         echo "<h1>$name has reached Milestone 1!!!</h1>";
     } catch (Exception $e) {
+        # show any errors.
         echo "<h1>Error: {$e->getMessage()}</h1>";
     }
 
+    # Show the container's hostname on the web page.
     echo "<p>Container: <code>$hostname</code></p>";
 
+    # Close the connection to the database.
     mysqli_close($conn);
     ?>
 </body>
